a01322f26267ca448fc3203dddc1d2d8ecf65d3e
Jieruei Chang <knosmos@gmail.com>
2024-11-05 15:51:38 -0500

    add requirements.txt

--- test_client.py
+++ test_client.py
@@ -1,35 +1,3 @@
-'''
-import asyncio
-import socketio
-
-sio = socketio.AsyncClient()
-name = input("name > ")
-
-@sio.event
-async def connect():
-    print('connection established')
-    await sio.emit("b", {"sender": name, "msg": "hello world!"})
-
-@sio.event
-async def state_update(data):
-    print('message received with ', data)
-    await sio.emit('my response', {'response': 'my response'})
-
-@sio.event
-async def disconnect():
-    print('disconnected from server')
-
-async def main():
-    await sio.connect('http://localhost:7777/')
-    while True:
-        s = input("state > ")
-        await sio.emit("b", {"sender": name, "msg": s})
-        print("sent!")
-
-if __name__ == '__main__':
-    asyncio.run(main())
-'''
-
 import socketio
 
 sio = socketio.Client()
